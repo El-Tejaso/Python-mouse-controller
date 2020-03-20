7be69d6bde616b0a3ee31860af0a7aa928165433
Tejas Hegde <45031546+El-Tejaso@users.noreply.github.com>
2020-03-20 20:17:23 +1030

    Improved structure, gave credit

--- pythoncontrolmouse.py
+++ pythoncontrolmouse.py
@@ -1,15 +1,12 @@
 import pyautogui
+
+#allows mouse to go to corners of the screen without breaking program
+pyautogui.FAILSAFE = False
+
 import win32api
 import time
 
-def keyWasUnPressed():
-    print("enabling joystick...")
-    #enable joystick here
-
-def keyWasPressed():
-    print("disabling joystick...")
-    #disable joystick here
-
+#full credit to Kevin from stackoverflow here: https://stackoverflow.com/questions/13380938/pygame-capture-keyboard-events-when-window-not-in-focus
 def isKeyPressed(key):
     #"if the high-order bit is 1, the key is down; otherwise, it is up."
     return (win32api.GetKeyState(key) & (1 << 7)) != 0
@@ -17,44 +14,56 @@ def isKeyPressed(key):
 
 #Look up virtual keys here: https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes?redirectedfrom=MSDN
 
-LEFT_ARROW = ord('J')#0x25
-UP_ARROW = ord('I')#0x26
-RIGHT_ARROW = ord('L')#0x27
-DOWN_ARROW = ord('K')#0x28
+LEFT_ARROW = 0x25
+UP_ARROW = 0x26
+RIGHT_ARROW = 0x27
+DOWN_ARROW = 0x28
 SHIFT = 0xA0
 CTRL = 0xA2
 
-#allows mouse to go to corners of the screen without breaking program
-pyautogui.FAILSAFE = False
+#program key binds
+
+moveLeftKey = ord('J')
+moveRightKey = ord('L')
+moveUpKey = ord('I')
+moveDownKey = ord('K')
+slowdown1Key = CTRL
+slowdown2Key = SHIFT
+
 
 def move(x, y):
     if(abs(x)+abs(y) > 0.01):
         print("moving mouse {0}, {1}".format(x,y))
     pyautogui.moveRel(x,y)
 
-base_speed = 100.0
+
+speed = 200.0
+
+
+print("program started")
 
 while True:
     x = 0.0
     y = 0.0
 
-    if(isKeyPressed(LEFT_ARROW)):
-        x -= base_speed
+    if(isKeyPressed(moveLeftKey)):
+        x -= speed
     
-    if(isKeyPressed(RIGHT_ARROW)):
-        x += base_speed
+    if(isKeyPressed(moveRightKey)):
+        x += speed
 
-    if(isKeyPressed(DOWN_ARROW)):
-        y += base_speed
+    if(isKeyPressed(moveDownKey)):
+        y += speed
 
-    if(isKeyPressed(UP_ARROW)):
-        y -= base_speed
+    if(isKeyPressed(moveUpKey)):
+        y -= speed
 
-    if(isKeyPressed(SHIFT)):
+    if(isKeyPressed(slowdown1Key)):
+        x*=0.75
+        y*=0.75
+
+    if(isKeyPressed(slowdown2Key)):
         x*=0.5
         y*=0.5
-    elif(isKeyPressed(CTRL)):
-        x*=2
-        y*=2
     
     move(x, y)
